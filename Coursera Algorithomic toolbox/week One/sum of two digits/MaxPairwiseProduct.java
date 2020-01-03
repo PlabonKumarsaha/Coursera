3467eced73e84b1083adde5e22fed26deeaf3d0d
Plabon <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-01-03 23:15:00 +0600

    Update MaxPairwiseProduct.java

--- Coursera Algorithomic toolbox/week One/sum of two digits/MaxPairwiseProduct.java
+++ Coursera Algorithomic toolbox/week One/sum of two digits/MaxPairwiseProduct.java
@@ -1,58 +1,72 @@
-import java.util.*;
-import java.io.*;
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
-public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
-        int max_product = 0;
-        int n = numbers.length;
+using std::vector;
+using std::cin;
+using std::cout;
 
-        for (int first = 0; first < n; ++first) {
-            for (int second = first + 1; second < n; ++second) {
-                max_product = max(max_product,
-                    numbers[first] * numbers[second]);
+long long MaxPairwiseProduct(const vector<int> &numbers) {
+    long long result = 0;
+    int n = numbers.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (((long long) (numbers[i])) * numbers[j] > result) {
+                result = ((long long) (numbers[i])) * numbers[j];
             }
         }
+    }
+    return result;
+}
 
-        return max_product;
+long long MaxPairwiseProductFast(const vector<int> &numbers) {
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for (int i = 0; i < n; ++i)
+        if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+            max_index1 = i;
+
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+        if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+            max_index2 = j;
+
+    //cout << max_index1 << ' ' << max_index2 << "\n";
+
+    return ((long long) (numbers[max_index1])) * numbers[max_index2];
+}
+
+int main() {
+    // while (true) {
+    //   int n = rand() % 1000 + 2;
+    //   cout << n << "\n";
+    //   vector<int> a;
+    //   for (int i = 0; i < n; ++i) {
+    //     a.push_back(rand() % 100000);
+    //   }
+    //   for (int i = 0; i < n; ++i) {
+    //     cout << a[i] << ' ';
+    //   }
+    //   cout << "\n";
+    //   long long res1 = MaxPairwiseProduct(a);
+    //   long long res2 = MaxPairwiseProductFast(a);
+    //   if (res1 != res2) {
+    //     cout << "Wrong answer: " << res1 << ' ' << res2 << "\n";
+    //     break;
+    //   }
+    //   else {
+    //     cout << "OK\n";
+    //   }
+    // }
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
     }
 
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        System.out.println(getMaxPairwiseProduct(numbers));
-    }
-
-    static class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastScanner(InputStream stream) {
-            try {
-                br = new BufferedReader(new
-                    InputStreamReader(stream));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
-
+    long long result = MaxPairwiseProductFast(numbers);
+    cout << result << "\n";
+    return 0;
 }
